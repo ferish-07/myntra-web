084cc21193df518c1be9bfd2a1f48b6e50ae05ea
FerishModi1507 <Ferish.Modi@srkay.com>
2024-06-06 22:26:30 +0530

    new url

--- src/screens/redux/Urls.js
+++ src/screens/redux/Urls.js
@@ -1,14 +1,14 @@
 export const API_REGISTER =
   "https://myntra-backend-five.vercel.app/api/auth/register";
-
+// https://myntra-backend-five.vercel.app/api/categories/getSectionData
 export const API_ADD_MAIN_CATEGORY =
-  "https://myntra-backend-five.vercel.app/categories/add/main/category";
+  "https://myntra-backend-five.vercel.app/api/categories/add/main/category";
 
 export const API_GET_SECTION_DATA =
-  "https://myntra-backend-five.vercel.app/categories/getSectionData";
+  "https://myntra-backend-five.vercel.app/api/categories/getSectionData";
 
 export const API_ADD_SECTION =
-  "https://myntra-backend-five.vercel.app/categories/add/section";
+  "https://myntra-backend-five.vercel.app/api/categories/add/section";
 
 export const API_ADD_SUB_SECTION =
-  "https://myntra-backend-five.vercel.app/categories/add/sub/section";
+  "https://myntra-backend-five.vercel.app/api/categories/add/sub/section";
